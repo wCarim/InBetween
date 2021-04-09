0f89f4750dbb0a82249447f97dc4bdec73b66c5f
Waquil Carim <CARIM.FAISALWAQUIL@UDE.EDU.PH>
2021-04-09 14:37:15 +0800

    Revised the layout

--- InBetween/app/src/main/java/com/example/inbetween/MainActivity.java
+++ InBetween/app/src/main/java/com/example/inbetween/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private Button startbutt;
+    private Button startbutt, optbutt, helpbutt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +22,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        optbutt = (Button) findViewById(R.id.optbutt);
+        optbutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOptionsActivity();
+            }
+        });
+
+
+        helpbutt = (Button) findViewById(R.id.helpbutt);
+        helpbutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHelpActivity();
+            }
+        });
+
 
     }
     public void openActivity2() {
         Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+    public void openHelpActivity() {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+    public void openOptionsActivity() {
+        Intent intent = new Intent(this, OptionsActivity.class);
         startActivity(intent);
     }
 
